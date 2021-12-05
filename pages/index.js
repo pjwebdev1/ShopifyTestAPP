ccec25f4854013c6924106deb19b8a724401caff
pjwebdev1 <pjwebdev1@gmail.com>
2021-12-05 09:05:06 -0500

    save

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page, Button, Card, Stack, Badge, Thumbnail } from "@shopify/polaris";
 
 const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
+<Page
+title="General"
+divider={true} fullWidth={true} narrowWidth={true}
+primaryAction={
+<Button primary connectedDisclosure={{
+  accessibilityLabel: 'Other save actions',
+  actions: [{ content: 'Save as new' }]
+}}>Save</Button>}
+
+titleMetadata={<Badge status="success">Paid</Badge>}
+thumbnail={
+	<Thumbnail
+		source="https://burst.shopifycdn.com/photos/black-leather-choker-necklace_373x@2x.jpg"
+		alt="Black leather pet collar"
+	/>
+}
+>
+<p>Page content</p>
+
+
+
+<Card sectioned title="Fulfill order">
+    <Stack alignment="center">
+      <Stack.Item fill>
+        <p>Buy postage and ship remaining 2 items</p>
+      </Stack.Item>
+      <Button primary>Continue</Button>
+    </Stack>
+  </Card>
+
+</Page>
 );
 
 export default Index;
